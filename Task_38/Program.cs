eb846e2f51119504f2de47975fe8dc7139ab5341
Grigorov_Vasiliy <Grigorov@internet.ru>
2022-12-17 12:04:32 +0700

    38

---
+++ Task_38/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.WriteLine("Введите количество элементов массива: ");
+int n = int.Parse(Console.ReadLine());
+double[] array = new double[n];
+
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(1, 99);
+
+double sum = 0, max = 1, min = 99;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max)
+        max = array[i];
+    if (array[i] < min)
+        min = array[i];
+}
+sum = max - min;
+Console.WriteLine($"Случайный массив из чисел:\n[{string.Join("; ", array)}]");
+Console.WriteLine($"Разница между максимальным и минимальным элементом массива: {sum}");
