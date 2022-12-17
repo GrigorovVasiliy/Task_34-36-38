f4af263b0bc65834698982767eb6674999573ab0
Grigorov_Vasiliy <Grigorov@internet.ru>
2022-12-17 11:56:51 +0700

    34

---
+++ Task_34/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(100, 999);
+
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+    if (array[i] % 2 == 0)
+        count++;
+
+Console.WriteLine($"Случайный массив из трухзначных чисел:\n[{string.Join(", ", array)}]");
+Console.WriteLine($"Kоличество чётных чисел в массиве: {count}");
